a4579e35e6bfdb314461464cc61a8577242a075d
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2023-08-04 00:51:12 +0100

    Added a login view and updated the user-serializer

--- core/serializers.py
+++ core/serializers.py
@@ -7,3 +7,15 @@ class UserSerializer(ModelSerializer):
         model = User
         # fields = '__all__' # return all the values in model
         fields = ['id', 'first_name', 'last_name', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework import exceptions
+
 from .serializers import UserSerializer
+from .models import User
 
 
 class RegisterAPIView(APIView):
@@ -13,4 +15,21 @@ class RegisterAPIView(APIView):
 
         serializer = UserSerializer(data=data)
         serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return JsonResponse(serializer.data)
+
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+        if not user:
+            raise exceptions.AuthenticationFailed('Invalid credentials')
+
+        if not user.check_password(password):
+            raise exceptions.AuthenticationFailed('Invalid credentials')
+
+        serializer = UserSerializer(user)
         return JsonResponse(serializer.data)
