cc7a7764a7001f694090aa0f118060f42d8bc833
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2023-08-04 04:10:11 +0100

    Added a Reset password model

--- core/models.py
+++ core/models.py
@@ -18,3 +18,8 @@ class UserToken(models.Model):
     token = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     expired_at = models.DateTimeField()
+
+
+class Reset(models.Model):
+    email = models.CharField(max_length=255)
+    token = models.CharField(max_length=255, unique=True)
