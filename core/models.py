ddbc51c2b81066bc9cb17d1410794fcd6f00c20a
Odun Balogun <15163838+odbalogun@users.noreply.github.com>
2023-08-04 03:34:17 +0100

    Added UserToken model and migrations

--- core/models.py
+++ core/models.py
@@ -11,3 +11,10 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+
+class UserToken(models.Model):
+    user_id = models.IntegerField()
+    token = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expired_at = models.DateTimeField()
